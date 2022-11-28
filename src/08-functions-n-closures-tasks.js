48ee00c2f353d3200e05e73c7980791080e8eb4c
AlexKaroh <steam.alexkaroh@gmail.com>
2022-11-28 03:15:18 -0800

    solve: implement part of 08-functions-n-closures-tasks.js

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,11 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return function result(sum) {
+    const summ = f(g(sum));
+    return summ;
+  };
 }
 
 
@@ -44,8 +47,11 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function result(sum) {
+    const multi = sum ** exponent;
+    return multi;
+  };
 }
 
 
